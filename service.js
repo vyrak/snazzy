093df36edfd5731aa29a76b1deeddf73474c7ddb
vyrak bunleang <vyrak.bunleang@gmail.com>
2014-10-30 10:41:48 -0600

    add service js

---
+++ service.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var port = parseInt(process.env.PORT, 10) || 8001;
+var html = fs.readFileSync(path.join(process.cwd(), 'src', 'index.html'), {
+  encoding: 'utf8'
+});
+
+app.get('/', function(req, res) {
+  res.send(html);
+});
+
+var server = app.listen(port, function() {
+  console.log('Server started on port %d', server.address().port);
+});
