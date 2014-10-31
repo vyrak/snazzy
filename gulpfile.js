4f8ab49c89b73220b34173793c271475790080b0
vyrak bunleang <vyrak.bunleang@gmail.com>
2014-10-31 10:20:14 -0600

    double quotes in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,50 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var gutil = require('gulp-util');
-var webpack = require('gulp-webpack');
-var child_process = require('child_process');
+var child_process = require("child_process");
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var gutil = require("gulp-util");
+var webpack = require("gulp-webpack");
+var path = require("path");
 
 var child;
 
-gulp.task('webpack', function() {
-  return gulp.src('src/scripts/app.jsx')
+function stdout(data) {
+  gutil.log(gutil.colors.bgCyan(gutil.colors.black(data.toString().trim())));
+}
+
+function stderr(data) {
+  gutil.log(gutil.colors.bgRed(gutil.colors.white(data.toString().trim())));
+}
+
+gulp.task("webpack", function() {
+  return gulp.src("src/scripts/app.jsx")
     .pipe(webpack({
+      resolve: {
+        modulesDirectories: ["node_modules", "bower_components"]
+      },
       module: {
         loaders: [
-          {test: /\.jsx$/, loader: 'jsx-loader'}
+          {test: /\.jsx$/, loader: "jsx-loader"}
         ]
       }
     }))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public/scripts'));
+    .pipe(rename("app.js"))
+    .pipe(gulp.dest("public/scripts"));
 });
 
-gulp.task('express', function() {
+gulp.task("express", function() {
   if(child) {
     child.kill();
   }
-  child = child_process.spawn(process.execPath, ['./service.js'], {
+  child = child_process.spawn(process.execPath, ["./service.js"], {
     env: {
       NODE_ENV: process.env.NODE_ENV,
       PORT: process.env.PORT
     }
   });
-  child.stdout.on('data', function(data) {
-    gutil.log(gutil.colors.bgCyan(gutil.colors.black(data.toString().trim())));
-  });
-  child.stderr.on('data', function(data) {
-    gutil.log(gutil.colors.bgRed(gutil.colors.white(data.toString().trim())));
-  });
+  child.stdout.on("data", stdout);
+  child.stderr.on("data", stderr);
 
-  process.on('uncaughtException', function(err) {
+  process.on("uncaughtException", function(err) {
     if(child) {
       child.kill();
     }
@@ -44,7 +52,7 @@ gulp.task('express', function() {
   });
 });
 
-gulp.task('watch', ['webpack', 'express'], function() {
-  gulp.watch(['src/scripts/**/*.jsx', 'bower_components/**/*.js'], ['webpack']);
-  gulp.watch(['service.js'], ['express']);
+gulp.task("watch", ["webpack", "express"], function() {
+  gulp.watch(["src/scripts/**/*.jsx", "bower_components/**/*.js"], ["webpack"]);
+  gulp.watch(["service.js"], ["express"]);
 });
